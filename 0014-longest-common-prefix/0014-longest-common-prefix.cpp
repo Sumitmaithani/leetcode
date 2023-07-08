953ede02003bb5200bd582d8dc74d08cdc14b406
Sumit Maithani <86047367+Sumitmaithani@users.noreply.github.com>
2023-07-08 13:15:19 +0530

    Time: 9 ms (29.99%), Space: 9 MB (91.81%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -2,44 +2,17 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         string common="";
-        string commonArray[strs.size()-1];
-        string lastCommon="";
+        sort(strs.begin(),strs.end());
+        int len=strs.size();
         
-        if(strs.size()==1){
-            return strs[0];
-        }
+        string first=strs[0], last=strs[len-1];
         
-        for(int i=1; i<strs.size(); i++){
-            common="";
-            for(int j=0; j<strs[i].size(); j++){
-                 if(strs[i-1][j]!=strs[i][j]){
-                    break;
-                }
-                if(strs[i-1][j]==strs[i][j]){
-                    if(lastCommon==""){
-                        common+=strs[i-1][j];
-                    } else{
-                        if(lastCommon[j]==strs[i-1][j]){
-                            common+=strs[i-1][j];
-                        }
-                    }
-                    
-                }
+        for(int i=0; i<first.size(); i++){
+            if(first[i]!=last[i]){
+                return common;
             }
-            commonArray[i-1]=common;
-            lastCommon=common;
+            common+=first[i];
         }
-        int min=commonArray[0].size();
-        
-         for(int i=0; i<strs.size()-1; i++){
-             if(commonArray[i]==""){
-                 return "";
-             }
-             if(min>commonArray[i].size()){
-                 min=commonArray[i].size();
-                 common=commonArray[i];
-             }
-         }
         
         return common;
     }
