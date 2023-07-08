98f1123e97e99ba74f558439dce3673f2fca171f
Sumit Maithani <86047367+Sumitmaithani@users.noreply.github.com>
2023-07-08 11:47:24 +0530

    Time: 7 ms (33.23%), Space: 9.3 MB (21.57%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string common="";
+        string commonArray[strs.size()-1];
+        string lastCommon="";
+        
+        if(strs.size()==1){
+            common=strs[0];
+            return common;
+        }
+        
+        for(int i=1; i<strs.size(); i++){
+            common="";
+            for(int j=0; j<strs[i].size(); j++){
+                 if(strs[i-1][j]!=strs[i][j]){
+                    break;
+                }
+                if(strs[i-1][j]==strs[i][j]){
+                    if(lastCommon==""){
+                        common+=strs[i-1][j];
+                    } else{
+                        if(lastCommon[j]==strs[i-1][j]){
+                            common+=strs[i-1][j];
+                        }
+                    }
+                    
+                }
+            }
+            commonArray[i-1]=common;
+            lastCommon=common;
+        }
+        int min=commonArray[0].size();
+        
+         for(int i=0; i<strs.size()-1; i++){
+             if(commonArray[i]==""){
+                 return "";
+             }
+             if(min>commonArray[i].size()){
+                 min=commonArray[i].size();
+                 common=commonArray[i];
+             }
+         }
+        cout<<lastCommon;
+        
+        return common;
+    }
+};
