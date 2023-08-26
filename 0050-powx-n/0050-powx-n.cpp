27c6a70b60d7626cdec47f33c3258f1d9f79be59
Sumit Maithani <86047367+Sumitmaithani@users.noreply.github.com>
2023-08-26 19:07:41 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (14.56%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -6,7 +6,7 @@ public:
         if(nn<0) nn=-1*nn;
         
         while(nn>0){
-            if(nn%2==1){
+            if(nn%2){
                 ans*=x;
                 nn=nn-1;
             } else{
