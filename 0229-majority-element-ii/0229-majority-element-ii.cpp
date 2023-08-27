abe089dd0d99ad0d2c0614acaab5daf2aff8304c
Sumit Maithani <86047367+Sumitmaithani@users.noreply.github.com>
2023-08-27 19:06:25 +0530

    Time: 14 ms (43.76%), Space: 15.8 MB (93.36%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size(), mm=n/3+1;
-        map<int,int> ls;
+        int ct1=0,ct2=0,el1=0,el2=0, n=nums.size(),mm=n/3;
         vector<int> ans;
         
-        for(int i=0;i<n;i++){
-            ls[nums[i]]++;
-            if(mm==ls[nums[i]]){
-                ans.push_back(nums[i]);
+        for(int i=0; i<n;i++){
+            if(ct1==0 && el2!=nums[i]){
+                ct1=1;
+                el1=nums[i];
+            } else if(ct2==0 && el1!=nums[i]){
+                ct2=1;
+                el2=nums[i];
+            } 
+            else if(el1==nums[i]) ct1++;
+            else if(el2==nums[i]) ct2++;
+            else{
+                ct1--;
+                ct2--;
             }
+        }
+        
+        ct1=0,ct2=0;
+        for(int i=0; i<n; i++){
+            if(el1==nums[i]) ct1++;            
+            if(el2==nums[i]) ct2++;
+        }
+        
+        if(ct1>mm && el1!=el2){
+            ans.push_back(el1);
+        }
+        
+        if(ct2>mm){
+            ans.push_back(el2);
         }
         
         return ans;
