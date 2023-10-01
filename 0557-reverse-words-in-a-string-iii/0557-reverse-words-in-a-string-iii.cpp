a0208dccf293eaa56bf473399c808c171ce48c1e
Sumit Maithani <86047367+Sumitmaithani@users.noreply.github.com>
2023-10-01 20:27:39 +0530

    Time: 20 ms (27.72%), Space: 11.3 MB (41.78%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int start=0,end=0;
+        int start=0,end=0, n=s.size();
         string res;
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' ' || i==s.size()-1){
+        for(int i=0;i<n;i++){
+            if(s[i]==' ' || i==n-1){
                 for(int j=i; j>=start;j--){
-                    if(s[j]!=' '){
-                        res+=s[j];
-                    }
+                    if(s[j]!=' ') res+=s[j];
                 }
-                if(i!=s.size()-1) res+=' ';
+                if(i!=n-1) res+=' ';
                 start=i;
             }
         }
